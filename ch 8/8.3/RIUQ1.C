1f8ab5fbb03d188ef3c5f862474c53e9e33017e8
RiyaPatel2806 <riyampatel2806@gmail.com>
2023-11-06 20:12:37 +0530

    06/11/2023

---
+++ ch 8/8.3/RIUQ1.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int i,n,sum;
+	clrscr();
+
+	printf("enter n:");
+	scanf("%d",&n);
+
+	for(i=1;i<=n;a++)
+	{
+	   printf("%d\n",i);
+	   sum+=i;
+       }
+       printf("sum:%d",sum);
+
+ getch();
+ }
